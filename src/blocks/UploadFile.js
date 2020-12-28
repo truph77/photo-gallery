e52c1c340a041d96b974b3ede350fd52723bb4bc
Trung Pham <phamtrung2422k@gmail.com>
2020-12-28 17:18:26 +0700

    upload file

---
+++ src/blocks/UploadFile.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const UploadFile = styled.div`
+  .upload-file {
+    margin: 2rem;
+    text-align: center;
+  }
+`;
+
+export default UploadFile;
---
+++ src/components/UploadFile.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import UploadFile from "../blocks/UploadFile";
+
+const UploadFileComponents = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError(null);
+    } else {
+      setFile(null);
+      setError("Opps, Please choose an image file (png or jpeg)");
+    }
+  };
+
+  return (
+    <UploadFile>
+      <form className="upload-file">
+        <input type="file" onChange={changeHandler} />
+
+        <div className="outPut">
+          {error && <div className="error">{error}</div>}
+          {file && <div>{file.name}</div>}
+        </div>
+      </form>
+    </UploadFile>
+  );
+};
+
+export default UploadFileComponents;
